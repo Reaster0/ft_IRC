5bcf484c2c74dd3b824858e66ce296f14d9fcf7a
AlexDNZ <paleozene@gmail.com>
2022-03-21 17:04:38 +0100

    add irssi compatible ascii art

--- handlerFunction.cpp
+++ handlerFunction.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #define BUFFERMAX 512 //need to change accordingly
 
-const string g_welcome[] = 
+const string g_welcome2[] = 
 {"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
 "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⠤⠴⠒⠒⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠑⠒⠲⠦⢤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
 "⠀⠀⠀⠀⠀⢀⣀⡤⠒⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠒⠤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀",
@@ -43,11 +43,35 @@ const string g_welcome[] =
 "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠴⠖⠒⠒⠋⠉⠀⠀⠀⠀   ⠀",
 "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"};
 
+const string g_welcome[] = 
+{"     .... NO! .....FT_IRC SUCKS..... MNO! ...",
+"   ..... MNO!! ...................... MNNOO! ...",
+" ..... MMNO! ......................... MNNOO!! .",
+".... MNOONNOO!   MMMMMMMMMMPPPOII!   MNNO!!!! .",
+" ... !O! NNO! MMMMMMMMMMMMMPPPOOOII!! NO! ....",
+"    ...... ! MMMMMMMMMMMMMPPPPOOOOIII! ! ...",
+"   ........ MMMMMMMMMMMMPPPPPOOOOOOII!! .....",
+"   ........ MMMMMOOOOOOPPPPPPPPOOOOMII! ...",
+"    ....... MMMMM..    OPPMMP    .,OMI! ....",
+"     ...... MMMM::   o.,OPMP,.o   ::I!! ...",
+"        .... NNM:::.,,OOPM!P,.::::!! ....",
+"          .. MMNNNNNOOOOPMO!!IIPPO!!O! .....",
+"         ... MMMMMNNNNOO:!!:!!IPPPPOO! ....",
+"           .. MMMMMNNOOMMNNIIIPPPOO!! ......",
+"          ...... MMMONNMMNNNIIIOO!..........",
+"       ....... MN MOMMMNNNIIIIIO! OO ..........",
+"    ......... MNO! IiiiiiiiiiiiI OOOO ...........",
+"  ...... NNN.MNO! . O!!!!!!!!!O . OONO NO! ........",
+"   .... MNNNNNO! ...OOOOOOOOOOO .  MMNNON!........",
+"   ...... MNNNNO! .. PPPPPPPPP .. MMNON!........",
+"      ...... OO! ................. ON! .......",
+"         ................................"};
+
 void	welcome_ascii_displayer(UserIRC* user, Server& server)
 {
 	PayloadIRC temp;
 	int i(-1);
-    while (++i < 37)
+    while (++i < 21)
     {
      temp = PayloadIRC();
      temp.prefix = "EpikEkipEkolegram";
